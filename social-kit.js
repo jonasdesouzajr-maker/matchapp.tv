73134b4ad50f341767bd9694c1f99d5ec4a45e38
Jonas <jonasdesouzajr@gmail.com>
2026-09-18 06:31:56 -0300

    Social: update TikTok profile to @matchapp_ai

--- social-kit.js
+++ social-kit.js
@@ -2,18 +2,22 @@
 (function () {
   'use strict';
   const HANDLE = '@matchapp.tv';
+  const TIKTOK_HANDLE = '@matchapp_ai';
   const TAGS = '#MatchApp #MatchAppTV #WhatToWatch #OQueAssistir';
-  window.MATCHAPP_SOCIAL = { handle: HANDLE, tags: TAGS };
+  window.MATCHAPP_SOCIAL = { handle: HANDLE, tiktokHandle: TIKTOK_HANDLE, tags: TAGS };
   function paint() {
     const row = document.querySelector('.social-row');
     if (!row || row.dataset.socialKit === '1') return;
     row.dataset.socialKit = '1';
-    [['tiktok','TikTok','https://www.tiktok.com/@matchapp.tv'],['facebook','Facebook','https://www.facebook.com/matchapp.tv']].forEach(([cls,label,href]) => {
+    [
+      ['tiktok','TikTok','https://www.tiktok.com/@matchapp_ai',TIKTOK_HANDLE],
+      ['facebook','Facebook','https://www.facebook.com/matchapp.tv',HANDLE]
+    ].forEach(([cls,label,href,displayHandle]) => {
       if (row.querySelector('[href="'+href+'"]')) return;
       const a = document.createElement('a');
       a.className = 'social-btn social-'+cls; a.href = href; a.target = '_blank'; a.rel = 'noopener noreferrer';
       a.setAttribute('aria-label','MatchApp on '+label);
-      a.innerHTML = '<span>'+label+' '+HANDLE+'</span>';
+      a.innerHTML = '<span>'+label+' '+displayHandle+'</span>';
       row.appendChild(a);
     });
     if (!row.querySelector('.social-hash')) {
