5bbc1099989e17af98675b3d7ad4a2a5ee9735f7
Jonas <jonasdesouzajr@gmail.com>
2026-09-18 07:45:06 -0300

    Android: refresh production WebView build marker

--- android-studio/kidsapp/src/main/java/tv/matchapp/kids/MainActivity.kt
+++ android-studio/kidsapp/src/main/java/tv/matchapp/kids/MainActivity.kt
@@ -359,7 +359,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     companion object {
-        const val HOME = "https://matchapp.tv/kids/?utm_source=android_kids_app&appBuild=4"
+        const val HOME = "https://matchapp.tv/kids/?utm_source=android_kids_app&appBuild=5"
         const val APP_UA = "MatchAppTVAndroid/1.1.2 MatchAppAiKidsAndroid/1.1.2"
 
         private const val KIDS_APP_JS = """
