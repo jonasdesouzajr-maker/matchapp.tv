61f2f41805820d8cfcf6a1e24a4ff050ba15605e
Jonas <jonasdesouzajr@gmail.com>
2026-09-18 08:48:23 -0300

    Android Kids: sync exact-match preferences release

--- android-studio/kidsapp/src/main/java/tv/matchapp/kids/MainActivity.kt
+++ android-studio/kidsapp/src/main/java/tv/matchapp/kids/MainActivity.kt
@@ -359,8 +359,8 @@ class MainActivity : AppCompatActivity() {
     }
 
     companion object {
-        const val HOME = "https://matchapp.tv/kids/?utm_source=android_kids_app&appBuild=7"
-        const val APP_UA = "MatchAppTVAndroid/1.1.5 MatchAppAiKidsAndroid/1.1.5"
+        const val HOME = "https://matchapp.tv/kids/?utm_source=android_kids_app&appBuild=8"
+        const val APP_UA = "MatchAppTVAndroid/1.1.6 MatchAppAiKidsAndroid/1.1.6"
 
         private const val KIDS_APP_JS = """
             (function(){
