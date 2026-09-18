ae0c3dcbcbf09607862bedf5ee6ce8ad81a96ce9
Jonas <jonasdesouzajr@gmail.com>
2026-09-18 07:21:41 -0300

    Android: refresh production build marker

--- android-studio/kidsapp/src/main/java/tv/matchapp/kids/MainActivity.kt
+++ android-studio/kidsapp/src/main/java/tv/matchapp/kids/MainActivity.kt
@@ -359,8 +359,8 @@ class MainActivity : AppCompatActivity() {
     }
 
     companion object {
-        const val HOME = "https://matchapp.tv/kids/?utm_source=android_kids_app&appBuild=3"
-        const val APP_UA = "MatchAppTVAndroid/1.1.1 MatchAppAiKidsAndroid/1.1.1"
+        const val HOME = "https://matchapp.tv/kids/?utm_source=android_kids_app&appBuild=4"
+        const val APP_UA = "MatchAppTVAndroid/1.1.2 MatchAppAiKidsAndroid/1.1.2"
 
         private const val KIDS_APP_JS = """
             (function(){
