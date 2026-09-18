4ccea0ace6ec3203eb038c7b155a2bc5a5b1addd
Jonas <jonasdesouzajr@gmail.com>
2026-09-18 18:01:41 -0300

    Bump Android release to 1.1.9

--- android-studio/app/build.gradle.kts
+++ android-studio/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "tv.matchapp.app"
         minSdk = 24
         targetSdk = 35
-        versionCode = 10
-        versionName = "1.1.8"
+        versionCode = 11
+        versionName = "1.1.9"
         resourceConfigurations += listOf("en", "pt", "es", "fr", "de", "it", "tr", "ru", "ar", "hi", "in", "ja", "ko", "zh")
     }
 
