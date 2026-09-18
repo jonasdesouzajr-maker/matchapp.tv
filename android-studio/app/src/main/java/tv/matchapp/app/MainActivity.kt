81cf35793579ea4066d4e08c3dd5e0decae0125c
Jonas <jonasdesouzajr@gmail.com>
2026-09-18 15:17:34 -0300

    Sync main Android WebView release marker

--- android-studio/app/src/main/java/tv/matchapp/app/MainActivity.kt
+++ android-studio/app/src/main/java/tv/matchapp/app/MainActivity.kt
@@ -374,8 +374,8 @@ class MainActivity : AppCompatActivity() {
     }
 
     companion object {
-        const val HOME = "https://matchapp.tv/?utm_source=android_app&appBuild=8"
-        const val APP_UA = "MatchAppTVAndroid/1.1.6 MatchAppAiAndroid/1.1.6"
+        const val HOME = "https://matchapp.tv/?utm_source=android_app&appBuild=9"
+        const val APP_UA = "MatchAppTVAndroid/1.1.7 MatchAppAiAndroid/1.1.7"
         private const val APP_MODE_JS = """
             (function(){
               window.MATCHAPP_IS_AD_FREE = true;
