ebb29097351c4ed56f29501658d4887b669db47d
Jonas <jonasdesouzajr@gmail.com>
2026-09-18 07:45:01 -0300

    Android: refresh production WebView build marker

--- android-studio/app/src/main/java/tv/matchapp/app/MainActivity.kt
+++ android-studio/app/src/main/java/tv/matchapp/app/MainActivity.kt
@@ -374,7 +374,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     companion object {
-        const val HOME = "https://matchapp.tv/?utm_source=android_app&appBuild=4"
+        const val HOME = "https://matchapp.tv/?utm_source=android_app&appBuild=5"
         const val APP_UA = "MatchAppTVAndroid/1.1.2 MatchAppAiAndroid/1.1.2"
         private const val APP_MODE_JS = """
             (function(){
