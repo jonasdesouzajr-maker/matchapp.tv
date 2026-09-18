5a8d0412ca6931730de26781c3c7e8f389ba96c3
Jonas <jonasdesouzajr@gmail.com>
2026-09-18 03:22:05 -0300

    Android Kids: add application class

---
+++ android-studio/kidsapp/src/main/java/tv/matchapp/kids/MatchAppApplication.kt
@@ -0,0 +1,13 @@
+package tv.matchapp.kids
+
+import android.app.Application
+import android.webkit.WebView
+
+class MatchAppApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        if (BuildConfig.DEBUG) {
+            WebView.setWebContentsDebuggingEnabled(true)
+        }
+    }
+}
