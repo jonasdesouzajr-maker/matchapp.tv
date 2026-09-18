81a2988223b0af4a7295cc70e2e54162b3972442
Jonas <jonasdesouzajr@gmail.com>
2026-09-18 15:17:44 -0300

    Sync Kids Android WebView release marker

--- android-studio/kidsapp/src/main/java/tv/matchapp/kids/MainActivity.kt
+++ android-studio/kidsapp/src/main/java/tv/matchapp/kids/MainActivity.kt
@@ -359,8 +359,8 @@ class MainActivity : AppCompatActivity() {
     }
 
     companion object {
-        const val HOME = "https://matchapp.tv/kids/?utm_source=android_kids_app&appBuild=8"
-        const val APP_UA = "MatchAppTVAndroid/1.1.6 MatchAppAiKidsAndroid/1.1.6"
+        const val HOME = "https://matchapp.tv/kids/?utm_source=android_kids_app&appBuild=9"
+        const val APP_UA = "MatchAppTVAndroid/1.1.7 MatchAppAiKidsAndroid/1.1.7"
 
         private const val KIDS_APP_JS = """
             (function(){
