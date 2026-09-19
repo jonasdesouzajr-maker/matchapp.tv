d47851f51a5b34f443eb17bcdbac47e531b941de
Jonas <jonasdesouzajr@gmail.com>
2026-09-19 04:48:31 -0300

    Bump normal Android app for check-in release

--- android-studio/app/build.gradle.kts
+++ android-studio/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "tv.matchapp.app"
         minSdk = 24
         targetSdk = 35
-        versionCode = 11
-        versionName = "1.1.9"
+        versionCode = 12
+        versionName = "1.1.10"
         resourceConfigurations += listOf("en", "pt", "es", "fr", "de", "it", "tr", "ru", "ar", "hi", "in", "ja", "ko", "zh")
     }
 
