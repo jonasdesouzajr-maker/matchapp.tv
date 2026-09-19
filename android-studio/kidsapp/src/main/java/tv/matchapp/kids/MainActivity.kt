6b76192b8a105613d54d059fe7ae090b81b42687
Jonas <jonasdesouzajr@gmail.com>
2026-09-19 10:53:29 -0300

    Sync Android apps to responsive UI release 1.1.12

--- android-studio/kidsapp/src/main/java/tv/matchapp/kids/MainActivity.kt
+++ android-studio/kidsapp/src/main/java/tv/matchapp/kids/MainActivity.kt
@@ -359,8 +359,8 @@ class MainActivity : AppCompatActivity() {
     }
 
     companion object {
-        const val HOME = "https://matchapp.tv/kids/?utm_source=android_kids_app&appBuild=13"
-        const val APP_UA = "MatchAppTVAndroid/1.1.11 MatchAppAiKidsAndroid/1.1.11"
+        const val HOME = "https://matchapp.tv/kids/?utm_source=android_kids_app&appBuild=14"
+        const val APP_UA = "MatchAppTVAndroid/1.1.12 MatchAppAiKidsAndroid/1.1.12"
 
         private const val KIDS_APP_JS = """
             (function(){
