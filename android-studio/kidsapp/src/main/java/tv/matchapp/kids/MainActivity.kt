0bb122df5a6721e9f0d272665f5304c13bd1fed8
Jonas <jonasdesouzajr@gmail.com>
2026-09-19 11:56:22 -0300

    Sync Android apps to premium2 UI release 1.1.14

--- android-studio/kidsapp/src/main/java/tv/matchapp/kids/MainActivity.kt
+++ android-studio/kidsapp/src/main/java/tv/matchapp/kids/MainActivity.kt
@@ -359,8 +359,8 @@ class MainActivity : AppCompatActivity() {
     }
 
     companion object {
-        const val HOME = "https://matchapp.tv/kids/?utm_source=android_kids_app&appBuild=15"
-        const val APP_UA = "MatchAppTVAndroid/1.1.13 MatchAppAiKidsAndroid/1.1.13"
+        const val HOME = "https://matchapp.tv/kids/?utm_source=android_kids_app&appBuild=16"
+        const val APP_UA = "MatchAppTVAndroid/1.1.14 MatchAppAiKidsAndroid/1.1.14"
 
         private const val KIDS_APP_JS = """
             (function(){
