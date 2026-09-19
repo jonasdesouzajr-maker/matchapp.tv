4117ed0b096edf38934046b40bb0402169b4de09
Jonas <jonasdesouzajr@gmail.com>
2026-09-19 11:17:41 -0300

    Sync Android apps to premium UI release 1.1.13

--- android-studio/app/src/main/java/tv/matchapp/app/MainActivity.kt
+++ android-studio/app/src/main/java/tv/matchapp/app/MainActivity.kt
@@ -374,8 +374,8 @@ class MainActivity : AppCompatActivity() {
     }
 
     companion object {
-        const val HOME = "https://matchapp.tv/?utm_source=android_app&appBuild=14"
-        const val APP_UA = "MatchAppTVAndroid/1.1.12 MatchAppAiAndroid/1.1.12"
+        const val HOME = "https://matchapp.tv/?utm_source=android_app&appBuild=15"
+        const val APP_UA = "MatchAppTVAndroid/1.1.13 MatchAppAiAndroid/1.1.13"
         private const val APP_MODE_JS = """
             (function(){
               window.MATCHAPP_IS_AD_FREE = true;
