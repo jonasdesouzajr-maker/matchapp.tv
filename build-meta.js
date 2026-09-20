35d77fa2cc04cedfed76faa85ca69c29bf2fe71d
Jonas <jonasdesouzajr@gmail.com>
2026-09-20 20:09:26 -0300

    Refresh Home final wiring cache key

--- build-meta.js
+++ build-meta.js
@@ -112,7 +112,7 @@ window.MATCHAPP_BUILD = '2026.09.20.2';
 (function(){
   if(document.querySelector('script[data-matchapp-final-wiring]'))return;
   const s=document.createElement('script');
-  s.src='/final-wiring.js?v=20260920-design3';
+  s.src='/final-wiring.js?v=20260920-design4';
   s.async=false; s.defer=true; s.dataset.matchappFinalWiring='1';
   document.head.appendChild(s);
 })();
