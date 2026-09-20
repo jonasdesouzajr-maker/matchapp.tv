b77ae87a11bda4f0fca10c7b3306549ff7c21fd3
Jonas <jonasdesouzajr@gmail.com>
2026-09-20 04:44:34 -0300

    Point Android Kids app at release 20

--- android-studio/kidsapp/src/main/java/tv/matchapp/kids/MainActivity.kt
+++ android-studio/kidsapp/src/main/java/tv/matchapp/kids/MainActivity.kt
@@ -359,8 +359,8 @@ class MainActivity : AppCompatActivity() {
     }
 
     companion object {
-        const val HOME = "https://matchapp.tv/kids/?utm_source=android_kids_app&appBuild=19"
-        const val APP_UA = "MatchAppTVAndroid/1.1.17 MatchAppAiKidsAndroid/1.1.17"
+        const val HOME = "https://matchapp.tv/kids/?utm_source=android_kids_app&appBuild=20"
+        const val APP_UA = "MatchAppTVAndroid/1.1.18 MatchAppAiKidsAndroid/1.1.18"
 
         private const val KIDS_APP_JS = """
             (function(){
