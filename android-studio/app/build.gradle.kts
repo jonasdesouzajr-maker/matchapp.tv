627b44ba80d2f516d080dbd6a39d88c0b2bbffac
Jonas <jonasdesouzajr@gmail.com>
2026-09-22 07:31:07 -0300

    Add biometric guardian support to Android app

--- android-studio/app/build.gradle.kts
+++ android-studio/app/build.gradle.kts
@@ -47,6 +47,7 @@ android {
 dependencies {
     implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("androidx.biometric:biometric:1.1.0")
     implementation("androidx.activity:activity-ktx:1.9.3")
     implementation("androidx.webkit:webkit:1.12.1")
     implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
