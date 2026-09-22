4f5a1fcc139eb5a3ddce1e3d5ad677abd80adf6c
Jonas <jonasdesouzajr@gmail.com>
2026-09-21 21:04:59 -0300

    Align build metadata with Kids freeze fix

--- build-meta.js
+++ build-meta.js
@@ -1,5 +1,5 @@
 // Functional release identifier must match release.json.
-window.MATCHAPP_BUILD = '2026.09.21.3';
+window.MATCHAPP_BUILD = '2026.09.21.4';
 
 
 /* Search freshness + truthfulness layer. */
