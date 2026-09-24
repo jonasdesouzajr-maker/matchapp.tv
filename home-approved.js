50ce57998aefff5a0a7c924cdfeb4ce95c89b715
Jonas <jonasdesouzajr@gmail.com>
2026-09-24 15:30:44 -0300

    Refresh equalized desktop stack styling

--- home-approved.js
+++ home-approved.js
@@ -22,7 +22,7 @@
     approvedLink.rel = 'stylesheet';
     (document.head || document.documentElement).appendChild(approvedLink);
   }
-  approvedLink.href = '/home-approved.css?v=20260924-equalstack1';
+  approvedLink.href = '/home-approved.css?v=20260924-equalstack2';
   if (!document.getElementById('ma-install-onetap')) {
     var ot=document.createElement('script');
     ot.id='ma-install-onetap';
