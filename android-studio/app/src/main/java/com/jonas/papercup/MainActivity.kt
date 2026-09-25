3a3fcb76387a60266f3b3b20de7735e249cfcfd8
Jonas <jonasdesouzajr@gmail.com>
2026-09-25 12:12:09 -0300

    Bump adult Android Play release to code 33, version 1.1.31

--- android-studio/app/src/main/java/com/jonas/papercup/MainActivity.kt
+++ android-studio/app/src/main/java/com/jonas/papercup/MainActivity.kt
@@ -450,8 +450,8 @@ class MainActivity : AppCompatActivity() {
     }
 
     companion object {
-        const val HOME = "https://matchapp.tv/?utm_source=android_app&appBuild=32"
-        const val APP_UA = "MatchAppTVAndroid/1.1.30 MatchAppAiAndroid/1.1.30"
+        const val HOME = "https://matchapp.tv/?utm_source=android_app&appBuild=33"
+        const val APP_UA = "MatchAppTVAndroid/1.1.31 MatchAppAiAndroid/1.1.31"
         private const val APP_MODE_JS = """
             (function(){
               window.MATCHAPP_IS_AD_FREE = true;
